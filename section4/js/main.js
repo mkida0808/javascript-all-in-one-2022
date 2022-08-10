f414366b0de823bfa14e0e25e659ffdbb74ed5c7
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-10 17:47:36 +0900

    演算子の本質で知らなかったことを整理

--- section4/js/main.js
+++ section4/js/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
 {
-  
+  // 演算子の本質（論理和）
+  const test1 = 'hello' || 'hello2'; // 'hello' is true, 'hello2' is true.
+  console.log(test1);  // test1 is 'hello'
+
+  const test2 = 'hello' || ''; // 'hello' is true, '' is false.
+  console.log(test2);  // test2 is 'hello'
+
+  const test3 = '' || 'hello2'; // '' is false, 'hello2' is true.
+  console.log(test3);  // test3 is 'hello2'
+
+  const test4 = '' || false; // '' is false, false is false.
+  console.log(test4);  // test3 is false
+
+  // 演算子の本質（論理積）
+  const test5 = 'hello' && 'hello2'; // 'hello' is true, 'hello2' is true.
+  console.log(test5);  // test1 is 'hello2'
+
+  const test6 = 'hello' && 0; // 'hello' is true, '' is false.
+  console.log(test6);  // test2 is 0
+
+  const test7 = 0 && 'hello2'; // '' is false, 'hello2' is true.
+  console.log(test7);  // test3 is 0
+
+  const test8 = 0 && false; // '' is false, false is false.
+  console.log(test8);  // test3 is 0
+
+  // Null合体演算子
+  const test9 = false ?? 'user'; // falseはnullでもundefinedでもないので、''がtest9に代入される。
+  console.log(test9);  // ''
+
+  const test10 = null ?? 'user'; // nullはnull合体演算子の評価基準員合うので、'use'がtest10に代入される。
+  console.log(test10);  // ''
 }
