01234a6ea2dd336b802bb6688d6cbbea35e71a48
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-11 23:53:59 +0900

    クロージャーでプライベート変数を作成する例を実装

--- section7/js/main.js
+++ section7/js/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 {
-  
+  // クロージャーを用いらプライベント変数の作り方（例
+  let generatePerson = (name) => {
+    let age = 0;
+    return {
+      getName: () => name,
+      getAge: () => age,
+      incrementAge: () => {
+        age++;
+        // return age;
+      }
+    };
+  };
+  const makoto = generatePerson('makoto');
+  const tom = generatePerson('tom');
+  // makoto.name = 'ahoaho';
+  // makoto.age = 123123;
+  // console.log(makoto.incrementAge());
+  makoto.incrementAge();
+  makoto.incrementAge();
+  console.log(makoto.getAge());
+  console.log(makoto.getName());
+
+  tom.incrementAge();
+  tom.incrementAge();
+  tom.incrementAge();
+  tom.incrementAge();
+  console.log(tom.getAge());
+  console.log(tom.getName());
 }
