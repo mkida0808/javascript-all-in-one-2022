6ca5f56c1060df90141cd3dcead1b173226e9f30
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-11 14:37:33 +0900

    関数オブジェクト、関数式、オブジェクト内にメソッドを定義

--- section5/js/main.js
+++ section5/js/main.js
@@ -10,4 +10,25 @@
   console.log(add.length);
   // 関数をオブジェクト形式でコンソール表示
   console.dir(add);
+
+  // 名前付き関数式
+  let sayHi = function hi() {
+    return 'Hi';
+  };
+  console.log(sayHi());
+
+  // 無名関数
+  sayHi = function () {
+    return 'Hi（無名関数）'
+  }
+  console.log(sayHi());
+
+  // オブジェクトのキーの中に関数（メソッド）を書く
+  const person = {
+    name: 'makoto',
+    sayHi: function () {
+      return 'Hello（メソッド）';
+    }
+  };
+  console.log(person.sayHi());
 }
