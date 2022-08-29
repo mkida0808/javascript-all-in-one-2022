a5a3eed8b4fad78abe0848b359816526cc9b54ad
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-29 15:02:19 +0900

    実践的にあプロトタイプを操作する方法とその中でfor-inループとObject.keysの出力違いについて挙動確認

--- section9/js/main.js
+++ section9/js/main.js
@@ -14,6 +14,8 @@
   });
   console.log(Object.getPrototypeOf(obj)); // objプロトタイプのプロパティが見れる
 
+  console.log('----------');
+
   const obj2 = Object.create({ c: 7,}, { // obj2新規オブジェクトを作成して、プロトタイプに新規プロパティを追加する
     d: {
       value: 8,
@@ -26,4 +28,14 @@
   obj2.a = 5;
   obj2.b = 6;
   console.log(obj2);
+  for (const key in obj2) { // keyはプロトタイプのプロパティkeyまでループ検索する（結論：for-inループはほとんど使われない）
+    console.log(key);
+  }
+  console.log('---');
+  console.log(Object.keys(obj2)); // keyはプロトタイプのプロパティkeyまではループ検索しない
+
+  console.log('---');
+  for (const key of Object.keys(obj2)) { // keyはプロトタイプのプロパティkeyまではループ検索しない
+    console.log(key);
+  }
 }
